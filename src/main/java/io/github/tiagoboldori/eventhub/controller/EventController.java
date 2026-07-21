24bfc3b59e1379d78f3aa4c3d9952de5fb22b1f9
tiagoboldori <tiagoboldori@gmail.com>
2026-07-20 22:20:50 -0300

    continuacao front

--- src/main/java/io/github/tiagoboldori/eventhub/controller/EventController.java
+++ src/main/java/io/github/tiagoboldori/eventhub/controller/EventController.java
@@ -2,10 +2,13 @@ package io.github.tiagoboldori.eventhub.controller;
 
 import io.github.tiagoboldori.eventhub.dto.request.RegisterEventRequest;
 import io.github.tiagoboldori.eventhub.entity.Event;
+import io.github.tiagoboldori.eventhub.entity.User;
 import io.github.tiagoboldori.eventhub.repository.EventRepository;
+import io.github.tiagoboldori.eventhub.security.CustomUserDetails;
 import io.github.tiagoboldori.eventhub.service.EventService;
 import io.github.tiagoboldori.eventhub.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/events")
@@ -32,8 +37,15 @@ public class EventController {
     }
 
     @GetMapping("/register")
-    public String register(Model model){
+    public String register(Authentication authentication, Model model){
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        User user = userDetails.getUser();
+        model.addAttribute("loggedUser", user);
+
+
+
         model.addAttribute("request", new RegisterEventRequest());
+
         model.addAttribute("users", userService.listAll());
         return "events/register";
     }
